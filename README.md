b27e7c995cec8e1c130874206182da94c8159bad
Richard Adams <204173+richarda@users.noreply.github.com>
2020-11-06 03:45:58 -0500

    a little messy, but authenticated

---
+++ README.md
@@ -0,0 +1,12 @@
+create github app
+
+create private key
+
+create azure function
+
+local.settings.json
+
+"@octokit/auth-app":
+"@octokit/request"
+
+get installation id from url in settings
--- branch-protect/index.js
+++ branch-protect/index.js
@@ -1,4 +1,25 @@
+const { createAppAuth } = require("@octokit/auth-app");
+const { request } = require("@octokit/request");
+
 module.exports = async function (context, req) {
+    
+    const auth = createAppAuth({
+        appId: process.env["APP_ID"],
+        privateKey: process.env["PRIVATE_KEY"],
+        installationId: 12813104,
+    });
+
+    const { token, appId } = await auth({ type: "app" });
+
+    const requestWithAuth = request.defaults({
+        //headers: {
+        //authentication: `token ${token}`,
+        //},
+        request: {
+            hook: auth.hook,
+        },
+    });
+
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var created = req.body.action === "created";
@@ -6,6 +27,22 @@ module.exports = async function (context, req) {
 
     if (req.body.repository != null)
     {
+        var req_owner = req.body.repository.owner.login;
+        var req_repo = req.body.repository.name;
+        // the default branch parameter doesn't seem to be correct
+        // some repos are returning "master", but the api call to branches
+        // seems to expect "main"
+        var req_default_branch = req.body.repository.default_branch;
+
+        const result = await requestWithAuth("GET /repos/:owner/:repo/branches", {
+        //const result = await requestWithAuth("GET /orgs/:owner/repos", {
+            owner: req_owner,
+            repo: req_repo,
+            branch: 'main'
+        });
+
+        context.log(JSON.stringify(result.data))
+
         var id = req.body.repository.id;
         context.log(`repository id = ${id}`);
 
