a39c8a3536882fb6840b9174c02c76798346016f
Richard Adams <204173+richarda@users.noreply.github.com>
2020-11-06 04:41:23 -0500

    get installation id from request, delay, logs Add a delay if the branch isn't available immediately. Log out execution details.

--- branch-protect/index.js
+++ branch-protect/index.js
@@ -1,12 +1,13 @@
 const { createAppAuth } = require("@octokit/auth-app");
 const { request } = require("@octokit/request");
 
+
 module.exports = async function (context, req) {
     
     const auth = createAppAuth({
         appId: process.env["APP_ID"],
         privateKey: process.env["PRIVATE_KEY"],
-        installationId: 12813104,
+        installationId: req.body.installation.id,
     });
 
     const { token, appId } = await auth({ type: "app" });
@@ -25,7 +26,7 @@ module.exports = async function (context, req) {
     var created = req.body.action === "created";
     context.log(`repository created = ${created}`);
 
-    if (req.body.repository != null)
+    if (req.body.repository != null && created)
     {
         var req_owner = req.body.repository.owner.login;
         var req_repo = req.body.repository.name;
@@ -34,19 +35,38 @@ module.exports = async function (context, req) {
         // seems to expect "main"
         var req_default_branch = req.body.repository.default_branch;
 
-        const result = await requestWithAuth("GET /repos/:owner/:repo/branches", {
-        //const result = await requestWithAuth("GET /orgs/:owner/repos", {
+        context.log(`New repository detected: ${req_owner}/${req_repo}`);
+        try {
+            const result = await requestWithAuth("GET /repos/:owner/:repo/branches/:branch", {
+                owner: req_owner,
+                repo: req_repo,
+                branch: 'main'
+            });
+        } catch {
+            context.log("Retrying branch access after 3 seconds");
+            await new Promise(r => setTimeout(r, 3000));
+            const result = await requestWithAuth("GET /repos/:owner/:repo/branches/:branch", {
+                owner: req_owner,
+                repo: req_repo,
+                branch: 'main'
+            });
+        }
+
+        const result2 = await requestWithAuth("PUT /repos/:owner/:repo/branches/:branch/protection", {
             owner: req_owner,
             repo: req_repo,
-            branch: 'main'
+            branch: 'main',
+            required_status_checks: null,
+            enforce_admins: null,
+            required_pull_request_reviews: {
+                dismissal_restrictions: {},
+                dismiss_stale_reviews: true,
+                require_code_owner_reviews: true,
+            },
+            restrictions: null,
+
         });
 
-        context.log(JSON.stringify(result.data))
-
-        var id = req.body.repository.id;
-        context.log(`repository id = ${id}`);
-
-        var name = req.body.repository.name;
-        context.log(`repository name = ${name}`);
+        context.log(`Branch Protection applied for ${req_owner}/${req_repo}`)
     }
 }
